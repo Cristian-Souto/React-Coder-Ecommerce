3316c3a81c50fbd1dc11ea66b979c42de3aab289
cristian souto <souto.cristian@gmail.com>
2022-11-09 16:41:19 -0300

    gitignore package-lock.json

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../hoja-de-estilo/navbar.css"
+import CartWidget from "./CartWidget";
+
+const Navbar = () => {
+  return (      
+    <nav className="navbar">
+      <h1>BRAND</h1 >
+        <ul className="nav-items">
+          <li>
+            <a href="." className="active">Inicio</a>
+          </li>
+          <li>
+            <a href=".">Productos</a>
+          </li>
+          <li>
+            <a href=".">Contacto</a>
+          </li>
+        </ul>
+      <CartWidget />
+    </nav >
+   ); 
+} 
+ export default Navbar; 
