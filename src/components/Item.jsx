811e026b2036cf220144532959c0cc93dd9b3edf
cristian souto <souto.cristian@gmail.com>
2022-11-28 20:59:51 -0300

    se agrega estilos en el componente item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
 import '../hoja-de-estilo/container-items.css'
 import '../hoja-de-estilo/list-item.css'
+
 const Item = ({ product }) => {
   return (
     <div className="container-items">
-      <div className="list-item">
+      
         <img src={product.img} alt="keyboard" style={{ width: '150px' }} />
         <li>{product.name}</li>
         <li>{product.description}</li>
         {/*<Link to={`/item/${product.id}`}>
         <button>Detalle</button>
       </Link> */}
-        <p>Stock: {product.stock}</p>
-        <span style={{ fontWeight: '700' }}>${product.price}</span>
-      </div>
+        <p style={{fontSize:'12px',marginBottom:'5px'}}>Stock: {product.stock}</p>
+        <span style={{ fontWeight: '700'}}>${product.price}</span>
+     
     </div>
   )
 }
